97f1472f3700dbf3b508fa52d4b95321996df3fc
Lance Buenviaje <buenviajelance24@gmail.com>
2025-12-23 19:34:15 +0800

    committed

--- logic.js
+++ logic.js
@@ -2,8 +2,8 @@
                 document.getElementById('infoCard').style.display = "none";
             }
 
-            // PLAYER CARD LOGIC
-            function showCard(name, height, pos, team, reason) {
+            // PLAYER CARD LOGIC HOME
+            function showCard(name, almamater, pos, award, team, hometown, jerseyno, reason) {
                 const grid = document.getElementById('infoGrid');
                 const tags = document.getElementById('cardTags');
                 
@@ -15,16 +15,63 @@
                 tags.innerHTML = `<span class="tag rising">${pos}</span> <span class="tag prospect">${team}</span>`;
                 
                 grid.innerHTML = `
-                    <div class="info-item"><span>Height</span><strong>${height}</strong></div>
-                    <div class="info-item"><span>Position</span><strong>${pos}</strong></div>
-                    <div class="info-item"><span>Current Team</span><strong>${team}</strong></div>
+                    <div class="info-item"><span>Jersey Number</span><strong>${jerseyno}</strong></div>
+                    <div class="info-item"><span>Notable Award</span><strong>${award}</strong></div>
+                    <div class="info-item"><span>Hometown</span><strong>${hometown}</strong></div>
+                    <div class="info-item"><span>College</span><strong>${almamater}</strong></div>
                 `;
 
                 document.getElementById('infoCard').style.display = "flex";
             }
 
+            // PLAYER CARD LOGIC PLAYER RANKING
+            function showCardRanking(name, pos, award, team, hometown, jerseyno, draft, reason) {
+                const grid = document.getElementById('infoGrid');
+                const tags = document.getElementById('cardTags');
+                
+                document.getElementById('cardTitle').innerText = name;
+                document.getElementById('cardIcon').className = "fas fa-user-astronaut"; 
+                document.getElementById('descTitle').innerText = "Scouting Report";
+                document.getElementById('cardDescText').innerText = reason;
+
+                tags.innerHTML = `<span class="tag rising">${pos}</span> <span class="tag prospect">${team}</span>`;
+                
+                grid.innerHTML = `
+                    <div class="info-item"><span>Jersey Number</span><strong>${jerseyno}</strong></div>
+                    <div class="info-item"><span>Notable Award</span><strong>${award}</strong></div>
+                    <div class="info-item"><span>Hometown</span><strong>${hometown}</strong></div>
+                    <div class="info-item"><span>Draft Class</span><strong>${draft}</strong></div>
+
+                `;
+
+                document.getElementById('infoCard').style.display = "flex";
+            }
+
+            // PLAYER CARD LOGIC HQ
+            function showCardHQ(name, championship, pos, award, team, hometown, year, reason) {
+                const grid = document.getElementById('infoGrid');
+                const tags = document.getElementById('cardTags');
+                
+                document.getElementById('cardTitle').innerText = name;
+                document.getElementById('cardIcon').className = "fas fa-user-astronaut"; 
+                document.getElementById('descTitle').innerText = "Scouting Report";
+                document.getElementById('cardDescText').innerText = reason;
+
+                tags.innerHTML = `<span class="tag rising">${pos}</span> <span class="tag prospect">${team}</span>`;
+                
+                grid.innerHTML = `
+                    <div class="info-item"><span>No. of Year With National Team</span><strong>${year}</strong></div>
+                    <div class="info-item"><span>Notable Award</span><strong>${award}</strong></div>
+                    <div class="info-item"><span>Hometown</span><strong>${hometown}</strong></div>
+                    <div class="info-item"><span>Championship Ring</span><strong>${championship}</strong></div>
+                `;
+
+                document.getElementById('infoCard').style.display = "flex";
+            }
+
+
             // TEAM CARD LOGIC
-            function showTeamCard(teamName, stats, captain, coach, prevPos) {
+            function showTeamCard(teamName, setratio, captain, coach, league, championship) {
                 const grid = document.getElementById('infoGrid');
                 const tags = document.getElementById('cardTags');
 
@@ -33,17 +80,40 @@
                 document.getElementById('descTitle').innerText = "Season Notes";
                 document.getElementById('cardDescText').innerText = "This team has shown consistent growth in the current conference.";
 
-                tags.innerHTML = `<span class="tag underrated">Rank: ${prevPos}</span>`;
+                tags.innerHTML = `<span class="tag underrated">League: ${league}</span>`;
 
                 grid.innerHTML = `
                     <div class="info-item"><span>Head Coach</span><strong>${coach}</strong></div>
                     <div class="info-item"><span>Team Captain</span><strong>${captain}</strong></div>
-                    <div class="info-item"><span>Prev. Position</span><strong>${prevPos}</strong></div>
-                    <div class="info-item"><span>Team Stats</span><strong>${stats}</strong></div>
+                    <div class="info-item"><span>Championship</span><strong>${championship}</strong></div>
+                    <div class="info-item"><span>Set Ratio</span><strong>${setratio}</strong></div>
                 `;
 
                 document.getElementById('infoCard').style.display = "flex";
             }
+            // TEAM CARD LOGIC HOME
+            function showTeamCardHome(teamName, bestplayer, captain, coach, achievement, league) {
+                const grid = document.getElementById('infoGrid');
+                const tags = document.getElementById('cardTags');
+
+                document.getElementById('cardTitle').innerText = teamName;
+                document.getElementById('cardIcon').className = "fas fa-shield-halved";
+                document.getElementById('descTitle').innerText = "Season Notes";
+                document.getElementById('cardDescText').innerText = "This team has shown consistent growth in the current conference.";
+
+                tags.innerHTML = `<span class="tag underrated">Rank: ${achievement}</span>`;
+                tags.innerHTML += `<span class="tag underrated">League: ${league}</span>`;
+
+                grid.innerHTML = `
+                    <div class="info-item"><span>Head Coach</span><strong>${coach}</strong></div>
+                    <div class="info-item"><span>Team Captain</span><strong>${captain}</strong></div>
+                    <div class="info-item"><span>Notable Achievement</span><strong>${achievement}</strong></div>
+                    <div class="info-item"><span>Best Player</span><strong>${bestplayer}</strong></div>
+                `;
+
+                document.getElementById('infoCard').style.display = "flex";
+            }
+
 
             // Close if user clicks background
             window.onclick = function(event) {
